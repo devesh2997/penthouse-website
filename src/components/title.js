a3e0b60fc8f2f123db0355c59ac812a0f3882899
Devesh Anand <ananddevesh29.devp@outlook.com>
2019-05-29 21:57:49 +0530

    minor

--- src/components/title.js
+++ src/components/title.js
@@ -1,29 +1,39 @@
 import React from 'react';
-
+import MediaQuery from 'react-responsive';
 const The = () => (
 	<div>The</div>
 );
 
 const Penthouze = () => (
-	<div style={{fontSize:"6em"}}>Penthouze</div>
+	<div style={{ fontSize: "6em" }}>Penthouze</div>
 );
 
 const ContactSatvik = () => (
-	<span><a style={{textDecoration:"none", color:"black"}} href={"mailto:satviksinghal31@gmail.com"}>Satvik Singhal</a></span>
+	<span><a style={{ textDecoration: "none", color: "black" }} href={"mailto:satviksinghal31@gmail.com"}>Satvik Singhal</a></span>
 );
 
 const ContactDevesh = () => (
-	<span><a style={{textDecoration:"none", color:"black",}} href={"mailto:ananddevesh22@gmail.com"}>Devesh Anand</a></span>
+	<span><a style={{ textDecoration: "none", color: "black", }} href={"mailto:ananddevesh22@gmail.com"}>Devesh Anand</a></span>
 );
 
 const Contact = () => (
-	<div style={{marginTop:"25px", display:"flex", justifyContent:"space-between"}}><ContactSatvik /> <ContactDevesh/></div>
+	<div style={{ marginTop: "25px", display: "flex", justifyContent: "space-between" }}><ContactSatvik /> <ContactDevesh /></div>
 );
 
 const Title = () => (
-	<div style={{ position: "absolute", left: "50%", top: "50%", width:"500px", height:"auto", marginLeft:"-200px", marginTop:"-100px", fontFamily: 'Montserrat' }}>
-		<The /> <Penthouze />
-		<Contact />		
+	<div>
+		<MediaQuery minDeviceWidth={1224}>
+			<div style={{ display: "flex", flexDirection: "column", width: "50%", margin: "200px auto", fontFamily: 'Montserrat' }}>
+				<The /> <Penthouze />
+				<Contact />
+			</div>
+		</MediaQuery>
+		<MediaQuery maxDeviceWidth={1224}>
+			<div style={{ display: "flex", flexDirection: "column", width: "auto", margin: "200px auto", fontFamily: 'Montserrat' }}>
+				<The /> <Penthouze />
+				<Contact />
+			</div>
+		</MediaQuery>
 	</div>
 );
 
